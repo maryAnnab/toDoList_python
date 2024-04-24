fb318da46be935a32e1f7e0cfa3924dbbf860eb8
Anna Bielech <bielechanna@gmail.com>
2024-04-24 21:37:54 +0200

    Add DB

--- toDo.py
+++ toDo.py
@@ -1,75 +1,66 @@
-user_choice = -1
+import sqlite3
 
-tasks = []
+connection = sqlite3.connect("todo.db")
+
+def create_table(connection):
+    try:
+        cur = connection.cursor()
+        cur.execute("""CREATE TABLE task(task text)""")
+    except:
+        pass
+
+def show_tasks(connection):
+    cur = connection.cursor()
+    cur.execute("""SELECT task FROM task""")
+    result = cur.fetchall()
+
+    for row in result:
+        print(row[0])
 
 
-def show_tasks():
-    task_index = 0
-    for task in tasks:
-        print(task + " [" + str(task_index) + "]")
-        task_index += 1
 
-
-def add_task():
+def add_task(connection):
+    print("add task")
     task = input("Enter the content of your task: ")
     if task == "0":
-        print("Back to menu")
+       print("Back to menu")
     else:
-        tasks.append(task)
+        cur = connection.cursor()
+        cur.execute("""INSERT INTO task(task) VALUES(?)""", {task,})
         print("Task added!")
 
 
 def delete_task():
-    task_index = int(input("Enter the index of the task to be deleted: "))
-
-    if task_index < 0 or task_index > len(tasks) - 1:
-        print("A task with this index does not exist")
-        return
-
-    tasks.pop(task_index)
+    # task_index = int(input("Enter the index of the task to be deleted: "))
+    #
+    # if task_index < 0 or task_index > len(tasks) - 1:
+    #     print("A task with this index does not exist")
+    #     return
+    #
+    # tasks.pop(task_index)
     print("Task deleted!")
 
+create_table(connection)
 
-def save_tasks_to_file():
-    file = open("tasks.txt", "w")
-    for task in tasks:
-        file.write(task+"\n")
-    file.close()
+while True:
+    print()
+    print("1. Show task")
+    print("2. Add task")
+    print("3. Delete task")
+    print("4. Leave")
 
+    user_choice = int(input("Select number: "))
+    print()
 
-def load_tasks_from_file():
-    try:
-        file = open("tasks.txt")
-
-        for line in file.readlines():
-            tasks.append(line.strip())
-
-        file.close()
-    except FileNotFoundError:
-        return
-
-
-load_tasks_from_file()
-
-while user_choice != 5:
     if user_choice == 1:
-        show_tasks()
+        show_tasks(connection)
 
     if user_choice == 2:
-        add_task()
+        add_task(connection)
 
     if user_choice == 3:
         delete_task()
 
     if user_choice == 4:
-        save_tasks_to_file()
+        break
 
-    print()
-    print("1. Show task")
-    print("2. Add task")
-    print("3. Delete task")
-    print("4. Save differences in file")
-    print("5. Leave")
-
-    user_choice = int(input("Select number: "))
-    print()
